aac80e88a270dd3e6c401e8e9a76b1b4a55336e6
funkim <mcking172@skiff.com>
2024-01-07 11:12:04 -0500

    Computer Choice

---
+++ rock-paper.js
@@ -0,0 +1,11 @@
+function playRound(playerSelection, computerSelection){
+
+
+}
+function getComputerChoice() {
+    const randomElement = Hand[Math.floor(Math.random() * Hand.length)]
+}
+const Hand = ['Rock','Paper','Scissors']
+const computerSelection = getComputerChoice();
+const playerSelection = "rock";
+console.log(playRound(playerSelection, computerSelection))
