70df90885ee8f341145e8d731f44d7adf9fe7159
funkim <mcking172@skiff.com>
2024-01-08 16:48:33 -0500

    Setup most of base needed but have to connect button and the choice user gives.

--- rock-paper.js
+++ rock-paper.js
@@ -1,11 +1,53 @@
+
+const Hand = ['Rock','Paper','Scissors']
+const computerSelection = getComputerChoice();
+
 function playRound(playerSelection, computerSelection){
-
-
+    let playerSelection = document.querySelector('input[type=text]').value
+    function getComputerChoice() {
+    const randomElement = Hand[Math.floor(Math.random() * Hand.length)]
+    return randomElement;
+}
+    switch (playerSelection) {
+    case "Scissors":
+        if  (computerSelection === 'Rock' ) {
+            showdown = lose;
+        }
+        else if (computerSelection === 'Paper') {
+                showdown = win;
+        }
+        else {
+            showdown = tie
+        }
+        break;
+    case "Rock":
+        if (computerSelection === 'Rock') {
+            showdown = tie;
+        }
+        else if (computerSelection === 'Paper') {
+            showdown = lose
+        }
+        else {
+            showdown = win
+        }
+        break;
+    case "Paper":
+        if (computerSelection === 'Rock') {
+            showdown = win;
+        }
+        else if (computerSelection === 'Paper') {
+            showdown = tie;
+        }
+        else {
+            showdown = lose; 
+        }
+    } 
+    document.getElementById("result").innerHTML = showdown;
 }
 function getComputerChoice() {
     const randomElement = Hand[Math.floor(Math.random() * Hand.length)]
+    return randomElement;
 }
-const Hand = ['Rock','Paper','Scissors']
-const computerSelection = getComputerChoice();
-const playerSelection = "rock";
-console.log(playRound(playerSelection, computerSelection))
+const win = `You Won! They Chose ${computerSelection}.`
+const lose = `You lost =( They Chose ${computerSelection}.`
+const tie = `It was a tie! They also chose ${computerSelection}.`
