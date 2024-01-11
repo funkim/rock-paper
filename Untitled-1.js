d169962cce080f3434512065e5c6a4e6db369d07
funkim <mcking172@skiff.com>
2024-01-11 15:44:12 -0500

    Fixes

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,54 +1,66 @@
 const hand = ['Rock','Paper' ,'Scissors'];
-function playRound() {
-let   win = `You Won! They Chose ${computerSelection}.`
-let   lose = `You lost =( They Chose ${computerSelection}.`
-let   tie = `It was a tie! They also chose ${computerSelection}.`
-let showdown = 'The Result is...'
-    function computerSelection() {
-        const randomElement = hand[Math.floor(Math.random() * hand.length)]
-        return randomElement;
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let   win = `You Won! They Chose ${computerSelection}.`
+    let   lose = `You lost =( They Chose ${computerSelection}.`
+    let   tie = `It was a tie! They also chose ${computerSelection}.`
+    let showdown = 'The Result is...'
+    if (showdown === win) {
+        playerScore = playerScore++;
+    } else if (showdown === lose) {
+        computerScore = computerScore++;
     }
-    function playerSelection() {
-        document.querySelector('input[type=text]').value;
-        return value.toLowerCase();
-    }
-    switch (playerSelection()) {
-    case "scissors":
-        if  (computerSelection === 'Rock' ) {
-            showdown = lose;
+    function playRound() {
+        function computerSelector() {
+            const randomElement = hand[Math.floor(Math.random() * hand.length)]
+            return randomElement;
         }
-        else if (computerSelection === 'Paper') {
-        showdown = win;
+        let computerSelection = computerSelector();
+        function playerSelection() {
+            const value = document.querySelector('input[type=text]').value;
+            return value.toLowerCase();
         }
-        else {
-            showdown = tie
-        }
-        break;
-    case "rock":
-        if (computerSelection === 'Rock') {
-            showdown = tie;
-        }
-        else if (computerSelection === 'Paper') {
-            showdown = lose
-        }
-        else {
-            showdown = win
-        }
-        break;
-    case "paper":
-        if (computerSelection === 'Rock') {
+        switch (playerSelection()) {
+        case "scissors":
+            if  (computerSelection === 'Rock' ) {
+                showdown = lose;
+            }
+            else if (computerSelection === 'Paper') {
             showdown = win;
-        }
-        else if (computerSelection === 'Paper') {
-            showdown = tie;
-        }
-        else {
-            showdown = lose; 
-        }
-        break;
-            default:
-                showdown = "THATS NOT A ANSWER!!!!!"
-        break;
-    } 
+            }
+            else {
+                showdown = tie
+            }
+            break;
+        case "rock":
+            if (computerSelection === 'Rock') {
+                showdown = tie;
+            }
+            else if (computerSelection === 'Paper') {
+                showdown = lose
+            }
+            else {
+                showdown = win
+            }
+            break;
+        case "paper":
+            if (computerSelection === 'Rock') {
+                showdown = win;
+            }
+            else if (computerSelection === 'Paper') {
+                showdown = tie;
+            }
+            else {
+                showdown = lose; 
+            }
+            break;
+                default:
+                    showdown = "THATS NOT A ANSWER!!!!!"
+            break;
+        } 
+    }
+
+
     document.getElementById("result").innerHTML = showdown;
 }
