49440557cd6a8e739419d5d89e160e4a164e759d
funkim <mcking172@skiff.com>
2024-01-11 16:37:29 -0500

    Working game but both sides scores don't display properly on ui

--- Untitled-1.js
+++ Untitled-1.js
@@ -2,15 +2,19 @@ const hand = ['Rock','Paper' ,'Scissors'];
 function game() {
     let playerScore = 0;
     let computerScore = 0;
-    let   win = `You Won! They Chose ${computerSelection}.`
-    let   lose = `You lost =( They Chose ${computerSelection}.`
-    let   tie = `It was a tie! They also chose ${computerSelection}.`
-    let showdown = 'The Result is...'
-    if (showdown === win) {
-        playerScore = playerScore++;
-    } else if (showdown === lose) {
-        computerScore = computerScore++;
+    
+    if (playerScore < 3 && computerScore < 3) {
+        const roundResult = playRound();
+        if (roundResult === 'win') {
+            playerScore++;
+        } else if (roundResult === 'lose') {
+            computerScore++;
+        }
+    document.getElementById("result").innerHTML = `Player score = ${playerScore}, Computer score = ${computerScore}`;
+    console.log(roundResult)
+    const winner = playerScore === 3 ? "Player wins!" : "Computer wins!";
     }
+
     function playRound() {
         function computerSelector() {
             const randomElement = hand[Math.floor(Math.random() * hand.length)]
@@ -21,46 +25,50 @@ function game() {
             const value = document.querySelector('input[type=text]').value;
             return value.toLowerCase();
         }
+        let   win = `You Won! They Chose ${computerSelection}.`
+        let   lose = `You lost =( They Chose ${computerSelection}.`
+        let   tie = `It was a tie! They also chose ${computerSelection}.`
+        let showdown = 'The Result is...'
         switch (playerSelection()) {
         case "scissors":
             if  (computerSelection === 'Rock' ) {
-                showdown = lose;
+            return  showdown = lose;
             }
             else if (computerSelection === 'Paper') {
-            showdown = win;
+            return showdown = win;
             }
             else {
-                showdown = tie
+            return showdown = tie
             }
             break;
         case "rock":
             if (computerSelection === 'Rock') {
-                showdown = tie;
+                return showdown = tie;
             }
             else if (computerSelection === 'Paper') {
-                showdown = lose
+                return showdown = lose
             }
             else {
-                showdown = win
+                return showdown = win
             }
             break;
         case "paper":
             if (computerSelection === 'Rock') {
-                showdown = win;
+                return showdown = win;
             }
             else if (computerSelection === 'Paper') {
-                showdown = tie;
+                return showdown = tie;
             }
             else {
-                showdown = lose; 
+                return showdown = lose; 
             }
             break;
                 default:
-                    showdown = "THATS NOT A ANSWER!!!!!"
+                    return showdown = "THATS NOT A ANSWER!!!!!"
             break;
         } 
     }
 
 
-    document.getElementById("result").innerHTML = showdown;
+    document.getElementById("showdown").innerHTML 
 }
