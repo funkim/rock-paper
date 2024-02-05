61ddbf805d4d4635bd2d60cf052bb05b5dab0fd9
funkim <mcking172@skiff.com>
2024-02-05 16:21:44 -0500

    Improved readability / formatting (was going to rewrite the whole thing but recognized this probably wasn't important enough for that)

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,9 +1,9 @@
 const hand = ['Rock','Paper' ,'Scissors'];
+let playerScore = 0;
+let computerScore = 0;
+
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    if (playerScore < 3 && computerScore < 3) {
+    if (playerScore < 5 && computerScore < 5) {
         const roundResult = playRound();
         if (roundResult === 'win') {
             playerScore++;
@@ -12,63 +12,56 @@ function game() {
         }
     document.getElementById("result").innerHTML = `Player score = ${playerScore}, Computer score = ${computerScore}`;
     console.log(roundResult)
-    const winner = playerScore === 3 ? "Player wins!" : "Computer wins!";
+    const winner = playerScore === 5 ? "Player wins!" : "Computer wins!";
     }
-
-    function playRound() {
-        function computerSelector() {
-            const randomElement = hand[Math.floor(Math.random() * hand.length)]
-            return randomElement;
-        }
-        let computerSelection = computerSelector();
-        function playerSelection() {
-            const value = document.querySelector('input[type=text]').value;
-            return value.toLowerCase();
-        }
-        let   win = `You Won! They Chose ${computerSelection}.`
-        let   lose = `You lost =( They Chose ${computerSelection}.`
-        let   tie = `It was a tie! They also chose ${computerSelection}.`
-        let showdown = 'The Result is...'
-        switch (playerSelection()) {
-        case "scissors":
-            if  (computerSelection === 'Rock' ) {
-            return  showdown = lose;
-            }
-            else if (computerSelection === 'Paper') {
-            return showdown = win;
-            }
-            else {
-            return showdown = tie
-            }
-            break;
-        case "rock":
-            if (computerSelection === 'Rock') {
-                return showdown = tie;
-            }
-            else if (computerSelection === 'Paper') {
-                return showdown = lose
-            }
-            else {
-                return showdown = win
-            }
-            break;
-        case "paper":
-            if (computerSelection === 'Rock') {
-                return showdown = win;
-            }
-            else if (computerSelection === 'Paper') {
-                return showdown = tie;
-            }
-            else {
-                return showdown = lose; 
-            }
-            break;
-                default:
-                    return showdown = "THATS NOT A ANSWER!!!!!"
-            break;
-        } 
-    }
-
-
-    document.getElementById("showdown").innerHTML 
 }
+function playerSelection() {
+    const value = document.querySelector('input[type=text]').value;
+    return value.toLowerCase();
+}
+function playRound() {
+    function computerSelector() {
+        const randomChoice = hand[Math.floor(Math.random() * hand.length)]
+        return randomChoice;
+    }
+    let computerSelection = computerSelector();
+    let   win = `You Won! They Chose ${computerSelection}.`
+    let   lose = `You lost =( They Chose ${computerSelection}.`
+    let   tie = `It was a tie! They also chose ${computerSelection}.`
+        // These must be initiated here so cChoice can be defined within the string.
+    let showdown = 'The Result is...'
+    switch (playerSelection()) {
+    case "scissors":
+        if  (computerSelection === 'Rock' ) {
+        return  showdown = lose;
+        }
+        else if (computerSelection === 'Paper') {
+        return showdown = win;
+        }
+        else {
+        return showdown = tie
+        }
+    case "rock":
+        if (computerSelection === 'Rock') {
+            return showdown = tie;
+        }
+        else if (computerSelection === 'Paper') {
+            return showdown = lose
+        }
+        else {
+            return showdown = win
+        }
+    case "paper":
+        if (computerSelection === 'Rock') {
+            return showdown = win;
+        }
+        else if (computerSelection === 'Paper') {
+            return showdown = tie;
+        }
+        else {
+            return showdown = lose; 
+        }
+    } 
+}
+    document.getElementById("showdown").innerHTML 
+
